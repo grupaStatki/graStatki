272790def012a1b8ae62ff38276e97c1718b95c5
Bartłomiej Ch <202128@edu.p.lodz.pl>
2018-06-09 11:43:38 +0200

    Base for ship placement check

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,24 @@
-var display;
+//document elements handles and timer
+var display, gameTables, verifyButton;
 var duration = 3;
-var timer = duration, minutes, seconds;
-var height = 10;
-var width = 10;
-var t;
-
+var timer = duration,
+    minutes, seconds;
+//
+//game tables dimensions    
+var height = 10,
+    width = 10;
+//
+//ship placing variables
+var placingPhase = true;
+var shipSize = 4,
+    placedBlocks = 0;
+//
+//ship placement array    
+var playerArray = new Array(height);
+for (var i = 0; i < height; i++) {
+    playerArray[i] = new Array(width).fill(0);
+}
+//
 
 function startTimer() {
 
@@ -21,24 +35,29 @@ function startTimer() {
             display.textContent = "END";
         }
     }, 1000);
-	
-	
+
+
 }
 
 function resetTimer() {
     duration = 3;
-	timer = duration, minutes, seconds;
+    timer = duration, minutes, seconds;
 }
 
 
-function drawTable(){
+function drawTable() {
 
-    for(var el=0;el<t.length; el++){
-        for(var i=0;i<height;i++){
-            var column = t[el].insertRow(i);
+    for (var el = 0; el < gameTables.length; el++) {
+        for (var i = 0; i < height; i++) {
+            var column = gameTables[el].insertRow(i);
 
-            for(var j=0;j<width;j++){
+            for (var j = 0; j < width; j++) {
                 var cell = column.insertCell(j);
+                if (el == 0) {
+                    cell.id = 'p' + i + j;
+                } else {
+                    cell.id = 'o' + i + j;
+                }
                 cell.onclick = function () {
                     tableClick(this);
                 }
@@ -47,14 +66,56 @@ function drawTable(){
     }
 }
 
-function tableClick(tableCell){
-    tableCell.classList.toggle("click");
+function tableClick(tableCell) {
+
+    if (placingPhase) {
+        placeShipBlock(tableCell);
+    }
+}
+
+function placeShipBlock(tableCell) {
+    var x = tableCell.id.substring(1, 2);
+    var y = tableCell.id.substring(2, 3);
+
+    if (tableCell.classList != "click" && placedBlocks < shipSize) {
+        tableCell.classList.toggle("click");
+        if (tableCell.classList == "click") {
+            playerArray[x][y] = shipSize;
+            placedBlocks += 1;
+        }
+    } else if (tableCell.classList == "click") {
+        tableCell.classList.toggle("click");
+        playerArray[x][y] = 0;
+        placedBlocks -= 1;
+    }
+    if (placedBlocks == shipSize) {
+        verifyButton.disabled = false;
+    } else {
+        verifyButton.disabled = true;
+    }
+}
+
+function verifyShip(shipSize) {
+    console.log(playerArray);
+    //  console.log(playerArray[0][1]);
+
+
 }
 
 
+
+
+
+
+
+
+
+
 window.onload = function () {
-    t = document.getElementsByName("gameArea");
+    gameTables = document.getElementsByName("gameArea");
     display = document.querySelector('#time');
     drawTable();
     startTimer();
+    verifyButton = document.getElementById("verifyButton");
+    verifyButton.disabled = true;
 };

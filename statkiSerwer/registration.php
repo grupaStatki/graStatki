2176438cbb94be923c01c3a047b60ffdbf3089bd
Jacek Olszewski <202256@edu.p.lodz.pl>
2018-06-13 11:15:08 +0100

    Sprawdzenie przy rejestracji czy mail istnieje w bazie

--- statkiSerwer/registration.php
+++ statkiSerwer/registration.php
@@ -3,9 +3,7 @@ $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "statki";
-// Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
@@ -14,9 +12,15 @@ if(isset($_POST['submit']))
 {
 	$email = $_POST['email'];
 	$password = $_POST['password'];
-	echo $query = 'INSERT INTO usertable (email,password) VALUES (\''.$email.'\',\''.$password.'\')';
-	if($conn->query($query) === FALSE){
-		echo $conn->error;
+	$querymails = 'SELECT email FROM usertable WHERE email=\''.$email.'\'';
+	$mails = $conn->query($querymails);
+	if($mails->num_rows > 0){
+		echo "Błąd! Email już w bazie!";
+	} else {
+		$query = 'INSERT INTO usertable (email,password) VALUES (\''.$email.'\',\''.$password.'\')';
+		if($conn->query($query) === TRUE){
+			echo "Rejestracja udana!";
+		}
 	}
 }
 $conn->close();

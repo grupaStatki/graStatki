840d2ebc035bde1e1b185c5365d9a0e1b31f3fc9
Jacek Olszewski <202256@edu.p.lodz.pl>
2018-06-19 11:20:50 +0200

    Moved php to html files. Removed awkward page reloading after wrong combination, now it's showing message in the same page. Message formatting improvements. JS functions for writing messages.

--- loginPage.php
+++ loginPage.php
@@ -21,6 +21,11 @@
     <!-- Custom styles -->
     <link href="css/loginPage.css" rel="stylesheet">
 	
+	<script>
+	var wrongCombination = function() {
+		wrongCombinationBox.innerHTML = "<h5><font color=\"white\">Niepoprawna kombinacja loginu i hasła. Spróbuj ponownie!</font></h5>";
+	}
+	</script>
   </head>
 
   <body>
@@ -29,18 +34,45 @@
 
 		<div class="container">
 
-			<form class="form-signin" action="statkiSerwer/login.php" method="post">
+			<form class="form-signin" action="loginPage.php" method="post">
 				<h1 class="form-signin-heading">Statki</h1>
 				<input type="email" class="form-control" placeholder="Adres email" required="" autofocus="" name="email">
 				<input type="password" class="form-control" placeholder="Hasło" name="password" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Musi zawierać przynajmniej jedną cyfrę, jedną małą literę, jedną dużą literę, łącznie przynajmniej 8 znaków" required="">
 				<button class="btn btn-lg btn-success btn-block" type="submit" name="submit" id="submit">
 					Zaloguj się
 				</button>
-				<button class="btn btn-basic btn-block" type="button" onclick="window.location.href='registerPage.html'">
+				<button class="btn btn-basic btn-block" type="button" onclick="window.location.href='registerPage.php'">
 					Zarejestruj się
 				</button>
+				<div class="form-wrong-combination-message" id="wrongCombinationBox"> </div>
 			</form>
+			
 
 		</div>
+		<?php
+		$servername = "localhost";
+		$username = "root";
+		$password = "";
+		$dbname = "statki";
+		$conn = new mysqli($servername, $username, $password, $dbname);
+		if ($conn->connect_error) {
+			die("Connection failed: " . $conn->connect_error);
+		} 
+
+		if(isset($_POST['submit']))
+		{
+			$email = $_POST['email'];
+			$password = $_POST['password'];
+			$loginquery = 'SELECT email,password FROM usertable WHERE email=\''.$email.'\' AND password=\''.$password.'\'';
+			$logindetails = $conn->query($loginquery);
+			if($logindetails->num_rows > 0) {
+				header( "refresh:0; url=index.html" ); 
+			} else {
+				echo '<script type="text/javascript">','wrongCombination();','</script>';
+			}
+		}
+
+		$conn->close();
+		?>
   </body>
 </html>
